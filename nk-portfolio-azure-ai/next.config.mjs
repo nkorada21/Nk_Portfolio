158740528993233d6ca8badaf6245269372edbdd
Narendra Korada <narendrakorada.21@gmail.com>
2026-02-24 17:07:53 -0600

    Update next.config file

--- nk-portfolio-azure-ai/next.config.mjs
+++ nk-portfolio-azure-ai/next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import path from "path";
-
-const nextConfig = {
-    outputFileTracingRoot: path.resolve(
-    "C:/Users/naren/OneDrive/Desktop/Nk-Portfolio/Nk_Portfolio/nk-portfolio-azure-ai"
-  ),
-};
+const nextConfig = {};
 
 export default nextConfig;
-
-
-
-
-
